feb61de7dc14448867a9aea7882c407a970bc168
ferdi2005 <ferdi.traversa@gmail.com>
2025-07-05 01:27:49 +0200

    cap

--- config/puma.rb
+++ config/puma.rb
@@ -7,7 +7,7 @@ threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on
-port ENV.fetch("PORT", 3000)
+port ENV.fetch("PORT", 3001)
 
 # Specifies the `environment` that Puma will run in
 environment ENV.fetch("RAILS_ENV", "development")
