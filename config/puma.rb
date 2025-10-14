ea7ed1a6b899316486968c515f2ddc880f37e4e6
Ferdinando Traversa <ferdi.traversa@gmail.com>
2025-10-14 16:31:26 +0200

    Fix mailer

--- config/puma.rb
+++ config/puma.rb
@@ -7,7 +7,7 @@ threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on
-port ENV.fetch("PORT", 3001)
+port ENV.fetch("PUMA_PORT", 3001)
 
 # Specifies the `environment` that Puma will run in
 environment ENV.fetch("RAILS_ENV", "development")
