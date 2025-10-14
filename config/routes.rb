e210c97bae0758af400c87c9c68d2d30af4ec64b
Ferdinando Traversa <ferdi.traversa@gmail.com>
2025-10-14 16:10:47 +0200

    sidekiq web for debug

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+require "sidekiq/web"
 Rails.application.routes.draw do
   resources :vehicles
   resources :bank_accounts
@@ -52,6 +53,7 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  mount Sidekiq::Web, at: "/sidekiq"
   # Defines the root path route ("/")
   # root "posts#index"
 end
